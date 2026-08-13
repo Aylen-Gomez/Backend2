546822521b7154fb710182c188a6da2586351252
Aylen Gomez <aylen.gomez.cr@gmail.com>
2026-08-13 14:08:41 -0300

    refactor: centralizar configuracion de entorno

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-import "./config/env.js";
+import env from "./config/env.js";
 import app from "./app.js";
 import { connectDB } from "./config/database.js";
 
 connectDB();
 
-const PORT = process.env.PORT || 8080;
+const PORT = env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
