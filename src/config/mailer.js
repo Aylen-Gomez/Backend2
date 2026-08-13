896d503f24ee42f55291b621f043ded815bb9942
Aylen Gomez <aylen.gomez.cr@gmail.com>
2026-08-13 12:47:48 -0300

    refactor: usar config de entorno en mailer

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,12 +1,13 @@
 import nodemailer from "nodemailer";
+import env from "./env.js";
 
 const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: Number(process.env.MAIL_PORT),
+    host: env.MAIL_HOST,
+    port: env.MAIL_PORT,
     secure: false,
     auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
+        user: env.MAIL_USER,
+        pass: env.MAIL_PASS
     }
 });
 
