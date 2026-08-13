b564461ebf3d9879708b1b9c23542f0870ffa52a
Aylen Gomez <aylen.gomez.cr@gmail.com>
2026-08-13 12:46:53 -0300

    refactor: usar config de entorno en database

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import env from "./env.js";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(env.MONGO_URI);
         console.log("MongoDB conectado");
     } catch (error) {
         console.error("Error conectando MongoDB:", error.message);
