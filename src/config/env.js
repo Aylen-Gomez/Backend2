c6fc91673ae39949e27943f68caabb1bd105409d
Aylen Gomez <aylen.gomez.cr@gmail.com>
2026-08-21 16:19:17 -0300

    mejora configuracion de variables de entorno

--- src/config/env.js
+++ src/config/env.js
@@ -5,7 +5,7 @@ dotenv.config();
 const env = {
     PORT: process.env.PORT || 8080,
 
-    MONGO_URI: process.env.MONGO_URI,
+    MONGO_URI: process.env.MONGO_URL || process.env.MONGO_URI,
 
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1h",
